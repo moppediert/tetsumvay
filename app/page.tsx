93a89637632c121e0f9c38d5db49509c19842376
moppediert <moppediert@gmail.com>
2024-01-26 13:59:48 +0100

    hmmmmmm

--- app/page.tsx
+++ app/page.tsx
@@ -8,35 +8,54 @@ export default function Home() {
   const [thirdDigit, setThirdDigit] = React.useState(0);
 
   const [number, setNumber] = React.useState(0);
+  const [reset, setReset] = React.useState(true);
   const draw = () => {
+    setReset(false);
     // random between one and MAX
     setNumber(Math.floor(Math.random() * 150) + 1);
   };
+
   React.useEffect(() => {
-    let timer1 = setTimeout(async (num) => {
+    console.log("what");
+    const down = (event: KeyboardEvent) => {
+      event.preventDefault();
+      console.log(event.key);
+      if (event.key === " ") {
+        draw();
+      }
+    };
+    document.addEventListener("keydown", down);
+    return document.removeEventListener("keydown", down);
+  }, []);
+
+  React.useEffect(() => {
+    if (reset) return;
+    let counter = 0;
+    let timer1 = setInterval(() => {
+      console.log("what");
       let randomDigit = Math.floor(Math.random() * 10);
       setFirstDigit(randomDigit);
       randomDigit = Math.floor(Math.random() * 10);
       setSecondDigit(randomDigit);
       randomDigit = Math.floor(Math.random() * 10);
       setThirdDigit(randomDigit);
-
-      await new Promise((r) => setTimeout(r, 100));
-    }, 3 * 1000);
-
-    let digits = number.toString().split("").map(Number);
-    if (digits.length == 2) {
-      digits.splice(0, 0, 0);
-    } else if (digits.length == 1) {
-      digits.splice(0, 0, ...[0, 0]);
-    }
-    console.log(digits);
-    setFirstDigit(digits[0]);
-    setSecondDigit(digits[1]);
-    setThirdDigit(digits[2]);
-
-    clearTimeout(timer1);
+      counter += 1;
+      if (counter > 100) {
+        clearInterval(timer1);
+        let digits = number.toString().split("").map(Number);
+        console.log(digits);
+        if (digits.length == 2) {
+          digits.splice(0, 0, 0);
+        } else if (digits.length == 1) {
+          digits.splice(0, 0, ...[0, 0]);
+        }
+        setFirstDigit(digits[0]);
+        setSecondDigit(digits[1]);
+        setThirdDigit(digits[2]);
+      }
+    }, 30);
   }, [number]);
+
   return (
     <div className="flex flex-col justify-center items-center h-full bg-[#FFF5D7] gap-4">
       <div className="h-96 w-72 bg-[#B72526] rounded-3xl flex justify-center items-center">
